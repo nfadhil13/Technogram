0ea0a150612577c5e125c56b3a0d34962fb069ae
nfadhil13 <48423700+nfadhil13@users.noreply.github.com>
2020-12-31 21:35:54 +0700

    Home Not Added

--- app/src/main/java/com/fdev/technogram/ui/MainActivity.kt
+++ app/src/main/java/com/fdev/technogram/ui/MainActivity.kt
@@ -2,21 +2,36 @@ package com.fdev.technogram.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.runtime.Providers
 import androidx.compose.ui.platform.setContent
 import com.fdev.technogram.ui.screen.main.TechnogramMain
+import com.github.zsoltk.compose.backpress.AmbientBackPressHandler
+import com.github.zsoltk.compose.backpress.BackPressHandler
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
-
+    private val backPressHandler = BackPressHandler()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            TechnogramMain()
+            Providers(
+                AmbientBackPressHandler provides backPressHandler
+            ) {
+                TechnogramMain()
+            }
+
         }
 
+
+    }
+
+    override fun onBackPressed() {
+        if(!backPressHandler.handle()){
+            super.onBackPressed()
+        }
     }
 
 

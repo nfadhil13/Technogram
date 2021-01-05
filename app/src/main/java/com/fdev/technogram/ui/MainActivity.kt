28406985790b07ac82571d19d765c6716a17082b
nfadhil13 <48423700+nfadhil13@users.noreply.github.com>
2021-01-05 20:02:40 +0700

    Remove all unused libs and imports

--- app/src/main/java/com/fdev/technogram/ui/MainActivity.kt
+++ app/src/main/java/com/fdev/technogram/ui/MainActivity.kt
@@ -6,35 +6,19 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Providers
 import androidx.compose.ui.platform.setContent
 import com.fdev.technogram.ui.screen.main.TechnogramMain
-import com.github.zsoltk.compose.backpress.AmbientBackPressHandler
-import com.github.zsoltk.compose.backpress.BackPressHandler
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
-    private val backPressHandler = BackPressHandler()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Providers(
-                AmbientBackPressHandler provides backPressHandler
-            ) {
-                TechnogramTheme {
-                    TechnogramMain()
-                }
-            }
 
         }
 
 
-    }
-
-    override fun onBackPressed() {
-        if(!backPressHandler.handle()){
-            super.onBackPressed()
-        }
     }
 
 
